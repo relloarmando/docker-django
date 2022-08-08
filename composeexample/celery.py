a4e7937020a8f61ca5f1e0e248b9210ab1961dee
Armando Rello <rello.armando@gmail.com>
2022-08-08 13:12:50 -0700

    updated docker-compose.yml  db: condition: service_healthy

--- composeexample/celery.py
+++ composeexample/celery.py
@@ -9,7 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'composeexample.settings')
 app = Celery('celery_app')
 
 
-
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
@@ -21,7 +20,7 @@ app.conf.beat_schedule = {
     'celery.py_message': {
         'task': 'composeexample.tasks.celery_message',
         'schedule': 10.0,
-        'args': (['Programmed from celery.py'])
+        'args': (["Programmed from celery.py"])
     },
     'celery_example_tasks_add': {
         'task': 'celery_example.tasks.add',
@@ -32,6 +31,7 @@ app.conf.beat_schedule = {
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
--- composeexample/tasks.py
+++ composeexample/tasks.py
@@ -3,4 +3,5 @@ from celery import shared_task
 
 @shared_task
 def celery_message(message):
+    print(message)
     print(f'Celery Task {message}')
