f87ac8b6551940ffbc3e9a19aa5313413c8e09c6
Armando Rello <rello.armando@gmail.com>
2022-08-08 11:38:43 -0700

    updated docker compose and entry_script

--- entry_script.sh
+++ entry_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 celery -A composeexample worker --pool=solo --loglevel=info &
 celery -A composeexample beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
